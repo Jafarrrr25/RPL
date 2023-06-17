80aa0fdcd6b54b4906a8440b0d3523f2dde691b4
Ajitama38 <anjitama38@gmail.com>
2023-06-17 17:20:16 +0700

    commit

---
+++ app/Views/About.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container p-5 bg-dark text-warning text-center">
+        <img src="https://i.pinimg.com/564x/49/ac/3e/49ac3e04d48faea4cf82a2c56e0e91b8.jpg" class="img-fluid" alt="Anjay">
+        <h1>Rental Mali Anjay Mabar</h1>
+    </div>
+    <div class="container">
+        <div class="col">
+            <div class="row">
+                Program Ini Dibuat Oleh Kelompok 5:
+                Fajar Al najiim (215314006)
+                Muhammad Rizky Fajar Mali (215314014)
+                Basilius Arilla Dimas Nanggalih (215314019)
+                Ajitama Jaya (215314020)
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
