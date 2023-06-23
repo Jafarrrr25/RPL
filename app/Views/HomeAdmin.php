56aa85fbaa51bf3a342b44ecf8b69815a1f323db
Ajitama38 <anjitama38@gmail.com>
2023-06-23 14:16:49 +0700

    commit

--- app/Views/HomeAdmin.php
+++ app/Views/HomeAdmin.php
@@ -27,15 +27,15 @@
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Formulir Customer</a>
+                                <a class="nav-link" href="#">Verifikasi</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Kelola
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="/Mobil/dataMobil">Mobil</a></li>
-                                    <li><a class="dropdown-item" href="/dataSupir">Supir</a></li>
+                                    <li><a class="dropdown-item" href="mobil/showDataMobil">Mobil</a></li>
+                                    <li><a class="dropdown-item" href="supir/showDataSupir">Supir</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item">
