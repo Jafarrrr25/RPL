926f27fa025d16ae91c8d45d04392646bc0b4d80
BasiliusDimas019 <basiliusarilla@gmail.com>
2023-06-25 22:32:45 +0700

    commit

--- app/Views/Akun/Login.php
+++ app/Views/Akun/Login.php
@@ -33,6 +33,7 @@
 </body>
 <article>
     <div class="container mt-2">
+        <h1>Login</h1>
         <form action="http://localhost:8081/customer" method="post">
             <? csrf_field() ?>
             <div class="form-group">
