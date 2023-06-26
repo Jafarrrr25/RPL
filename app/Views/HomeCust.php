1a4fa157fc64c595973b71133840142d68b8a67b
Ajitama38 <anjitama38@gmail.com>
2023-06-26 07:59:31 +0700

    commit

--- app/Views/HomeCust.php
+++ app/Views/HomeCust.php
@@ -8,30 +8,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rental Mobil</title>
-    <link rel="stylesheet" href="/css/njajal.css">
+    <link rel="stylesheet" href="/css/stylecust.css">
 </head>
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar">
             <div class="container">
-                <a class="navbar-brand" href="#">Rental Mobil</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <a class="navbar-logo" href="#">Rental Mobil DVJR</a>
+                <div class="navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="formulir/formulir_Sewa">Sewa</a>
+                            <a href="formulir/formulir_Sewa">Sewa</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/about">About Us</a>
+                            <a href="/about">About Us</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/help">Help</a>
+                            <a href="/help">Help</a>
                         </li>
                     </ul>
                 </div>
