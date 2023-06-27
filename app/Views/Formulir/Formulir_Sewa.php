52dc286c3e008e3395e118cadbee6909bd58b377
Ajitama38 <anjitama38@gmail.com>
2023-06-27 16:15:09 +0700

    commit

--- app/Views/Formulir/Formulir_Sewa.php
+++ app/Views/Formulir/Formulir_Sewa.php
@@ -8,6 +8,12 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Formulir Sewa</title>
+    <style>
+        .flex-container {
+            display: flex;
+            justify-content: flex-end;
+        }
+    </style>
 </head>
 
 <body>
@@ -63,7 +69,9 @@
                 <label for="nim"><b>Tanggal Kembali :</b> </label>
                 <input type="date" class="form-control" name="tgl_pjk" id="tgl_pjk" placeholder="ID Kendaraan">
             </div>
-
+            <div class="flex-container">
+                <a href="/customer" class="btn btn-primary mt-2">Kembali</a>
+            </div>
             <a href="/Formulir/Sukses" class="btn btn-primary mt-2">Kirim</a>
             </form>
         </div>
